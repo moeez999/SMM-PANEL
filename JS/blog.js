8260af49c268eed0879c55c6d39a0db224932254
moeez999 <moeezraza69@gmail.com>
2024-06-25 00:00:10 +0500

    latest

---
+++ JS/blog.js
@@ -0,0 +1,260 @@
+const articles = [
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+  {
+    imageSrc: "./assets/articles.png",
+    title: "Rush of American users to buy from our site!",
+    description:
+      "Fallonik 7 golden tips to get more likes on Instagram that will help you to follow these things in time",
+  },
+
+  // Add more articles as needed
+];
+
+const itemsPerPage = 8;
+let currentPage = 1;
+
+function articlesSlide(imageSrc, title, description) {
+  return `
+    <div class="articles-cards p-2 rounded-4">
+      <img src="${imageSrc}" alt="">
+      <h6 class="fw-bold my-4">${title}</h6>
+      <p>${description}</p>
+      <div class="d-flex justify-content-center w-100">
+        <button class="btn rounded-5 see-more text-white px-4">
+          SEE MORE ...
+        </button>
+      </div>
+    </div>
+  `;
+}
+
+function renderArticlesCards(page = 1) {
+  const swiperWrapper = document.getElementById("articles-card-container");
+  swiperWrapper.innerHTML = ""; // Clear previous content
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedArticles = articles.slice(startIndex, endIndex);
+
+  paginatedArticles.forEach((article) => {
+    const slideHTML = articlesSlide(
+      article.imageSrc,
+      article.title,
+      article.description
+    );
+    swiperWrapper.innerHTML += slideHTML;
+  });
+}
+
+function renderPagination() {
+  const paginationWrapper = document.getElementById("pagination");
+  paginationWrapper.innerHTML = ""; // Clear previous content
+
+  const pageCount = Math.ceil(articles.length / itemsPerPage);
+
+  for (let i = 1; i <= pageCount; i++) {
+    const li = document.createElement("li");
+    li.classList.add("page-item");
+
+    if (i === currentPage) {
+      li.classList.add("active");
+    }
+    li.innerHTML = `<a class="page-link" href="#">${i}</a>`;
+    li.addEventListener("click", (e) => {
+      e.preventDefault();
+      currentPage = i;
+      renderArticlesCards(currentPage);
+      renderPagination(); // Call to update the active page in pagination
+    });
+    paginationWrapper.appendChild(li);
+  }
+}
+
+// Initial render
+renderArticlesCards(currentPage);
+renderPagination();
